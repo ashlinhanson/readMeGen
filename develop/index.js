f725ba9db79c1bda8b81935608bad3df288ac53b
ashlinhanson <ashlinhanson@gmail.com>
2020-09-18 19:56:43 -0500

    files, readme, questions

---
+++ develop/index.js
@@ -0,0 +1,59 @@
+// array of questions for user
+const questions = [
+    {
+        prompt: "Let's create your README!"
+    },
+    {
+        prompt: "What is the title of the project?",
+        answer: "title",
+    },
+    {
+        prompt: "What is the description of the project?",
+        answer: "description",
+    },
+    {
+        prompt: "What is the table of contents for the project?",
+        answer: "tableOfContents",
+    },
+    {
+        prompt: "What are the installation instructions for your project?",
+        answer: "installationInstructions",
+    },
+    {
+        prompt: "What is the usage information for the project",
+        answer: "usageInfo",
+    },
+    {
+        prompt: "What is the license for the project?",
+        answer: "license",
+    },
+    {
+        prompt: "What are the contribution guidelines for this project?",
+        answer: "contribution",
+    },
+    {
+        prompt: "What are the test instructions?",
+        answer: "testInstructions",
+    },
+    {
+        prompt: "What is your GitHub username?",
+        answer: "username",
+    },
+    {
+        prompt: "Finally, what is your email?",
+        answer: "email",
+    }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
