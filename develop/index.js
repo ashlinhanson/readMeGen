3b5e6dbd776db21a2e76f3273f411b3bbcedcec1
ashlinhanson <ashlinhanson@gmail.com>
2020-09-21 22:18:48 -0500

    markdown function

--- develop/index.js
+++ develop/index.js
@@ -2,8 +2,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 //questions for user to answer about their project
-inquirer
-.prompt([
+    inquire
+    .prompt([
     {
         prompt: "Let's create your README! What is the title of the project?",
         answer: "title",
@@ -51,15 +51,16 @@ inquirer
         answer: "email",
     },
     {
-        prompt: "Lastly, are there any special instructions you would like to add for the users?",
-        answer: "specialInstructions"
+        prompt: "Lastly, how would you like to be contacted from a potential client?",
+        answer: "contactInstructions"
     }
 
 ])
 
+
 .then(answers => {
     //compile all the users answers
-    const {title, description, technologies, installationInstructions, usageInfo, licenseOptions, licenseInfo, contribution, testInstructions, username, email, specialInstructions} = userAnswers
+    const {title, description, technologies, installationInstructions, usageInfo, licenseOptions, licenseInfo, contribution, testInstructions, username, email, contactInstructions} = userAnswers
 
     let license = "";
     let badge = "";
@@ -70,36 +71,43 @@ inquirer
             badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
             break;
         case "MIT":
-            license : `Copyright (c) ${licenseInfo} Licensed under the MIT license.`;
-            badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            license = `Copyright (c) ${licenseInfo} Licensed under the MIT license.`;
+            badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
             break;
         case "Boost Software License 1.0":
-            license: `Copyright (c) ${licenseInfo} Licensed under the Boost Software license.`;
-            badge: "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+            license = `Copyright (c) ${licenseInfo} Licensed under the Boost Software license.`;
+            badge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
             break;
         case "Mozilla Public License 2.0":
-            license : `Copyright (c) ${licenseInfo} Licensed under the Mozilla Public license.`;
-            badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+            license = `Copyright (c) ${licenseInfo} Licensed under the Mozilla Public license.`;
+            badge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
             break;
         case "Apache License 2.0":
-            license: `Copyright (c) ${licenseInfo} Licensed under the Apache license.`;
-            badge: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            license = `Copyright (c) ${licenseInfo} Licensed under the Apache license.`;
+            badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
             break;
         case "The Unlicense":
-            license: `Copyright (c) ${licenseInfo} Licensed under the Unlicense license.`;
-            badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+            license = `Copyright (c) ${licenseInfo} Licensed under the Unlicense license.`;
+            badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
             break;
     }
-})
-
+});
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function generateMarkdown(answers) {
+    return `# ${title} ${badge} \n ${description} \n ## Table of Contents \n * [Installation Instructions](#Installation-Instructions) \n * [Instructions for Use](#Instructions-for-Use) \n * [Contribution Guidelines](#Contribution-Guidelines) \n * [Testing Instructions](#Testing-Instructions) \n * [Questions](#Questions) \n * [License](#License) \n ## Installation Instructions \n ${installationInstructions} \n ## Instructions for Use \n ${usageInfo} \n ## Contribution Guidelines  \n ${contribution} \n ## Testing Instructions \n ${testInstructions} \n ## Questions  \n * Github Username: ${username} \n * Email: ${email} \n * How to contact: ${contactInstructions} \n ## License \n ${license}`;
+};
 
 // function to initialize program
 function init() {
+    try {
+        const userAnswers = await readmeQuestions();
+        generateReadme(answers);
+        writeToFile("README.md", generateReadme(answers));
+    } catch (err){
+        console.log(err);
+    }
 
-}
+};
 
 // function call to initialize program
 init();
